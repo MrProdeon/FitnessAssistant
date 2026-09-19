e443e065c854845fa6d6453025dd298de8643241
Prodeon <prodeon21@gmail.com>
2026-09-19 14:08:26 +0300

    create app for users

---
+++ users/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Электронная почта должна быть обязательно указана")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_passord(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Супер-пользователь должен иметь is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Супер-пользователь должен иметь is_superuser=True")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, verbose_name="Номер телефона")
+    email = models.EmailField(max_length=100, unique=True, blank=False, null=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
