b89b5f5f7adc9a6f99796f50f636707c0f2669e2
Prodeon <prodeon21@gmail.com>
2026-09-20 11:56:29 +0300

    crate view for list and create user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics
+from users.models import CustomUser
+from users.serializers import CustomUserSerializer
+
 
 # Create your views here.
+
+class UserListCreateAPIView(generics.ListCreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
