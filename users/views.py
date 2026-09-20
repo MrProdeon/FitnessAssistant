0f8cb51fa16ab6e8657846e1d340e773f3caa795
Prodeon <prodeon21@gmail.com>
2026-09-20 12:59:49 +0300

    add permissions in users views

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+
 from users.models import CustomUser
+from users.permissions import IsSelfOrAdmin
 from users.serializers import CustomUserSerializer
 
 
@@ -9,15 +12,24 @@ from users.serializers import CustomUserSerializer
 class UserListCreateAPIView(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = []
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [IsAuthenticated()]
+        return [AllowAny()]
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
 
 class UserUpdateAPIView(generics.UpdateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated, IsSelfOrAdmin]
 
 class UserDestroyAPIView(generics.DestroyAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated, IsSelfOrAdmin]
